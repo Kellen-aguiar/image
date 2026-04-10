8e4f103d7572ae86b54f66e935639b7b1c15983e
Kellen-aguiar <kellen.a@estudante.ifmt.edu.br>
2026-04-10 11:26:48 -0500

    10/04/26

--- image/demo/src/main/java/io/spring/image/demo/controller/ImagesController.java
+++ image/demo/src/main/java/io/spring/image/demo/controller/ImagesController.java
@@ -1,5 +1,19 @@
 package io.spring.image.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/upload")
+@Slf4j
 public class ImagesController {
     //*
     // {"name": "", "size":100} //application/json
@@ -9,6 +23,7 @@ public class ImagesController {
     //*
 
     @PostMapping
+
     public ResponseEntity uploadImage(@RequestParam("file")  MultipartFile file,
                                       @RequestParam("name")String name,
                                       @RequestParam("tags") List<String> tags
@@ -33,7 +48,5 @@ public class ImagesController {
             log.error("Falha crítica ao processar imagem: ", e);
             return ResponseEntity.internalServerError().body("Erro no servidor");
         }
-
-
     }
 }
---
+++ image/demo/src/main/java/io/spring/image/demo/domain/enums/ImageExtension.java
@@ -0,0 +1,10 @@
+package io.spring.image.demo.domain.enums;
+
+public enum ImageExtension {
+
+    PING,
+    JPG,
+    GIF,
+    JPEG,
+    WebP
+}
