6a8fbb8e7f0d7d1729377cc710a38c406af94edb
Kellen-aguiar <kellen.a@estudante.ifmt.edu.br>
2026-04-23 09:57:47 -0500

    23/04/26

--- src/main/java/io/spring/image/demo/domain/enums/ImageExtension.java
+++ src/main/java/io/spring/image/demo/domain/enums/ImageExtension.java
@@ -1,10 +1,26 @@
 package io.spring.image.demo.domain.enums;
 
-public enum ImageExtension {
+import io.spring.image.demo.domain.entity.Image;
+import org.springframework.http.MediaType;
 
-    PING,
-    JPG,
-    GIF,
-    JPEG,
-    WebP
+import java.util.Arrays;
+
+public enum ImageExtension {
+    PNG (MediaType.IMAGE_PNG),
+    JPG (MediaType.IMAGE_JPEG),
+    GIF (MediaType.IMAGE_GIF),
+    JPEG (MediaType.IMAGE_JPEG);
+//    WebP (MediaType.IMAGE)
+
+    private MediaType mediaType;
+
+    ImageExtension(MediaType mediaType){
+        this.mediaType = mediaType;
+    }
+
+    public static ImageExtension valueOf(MediaType mediaType){
+        return Arrays.stream(values())
+                .filter(ie-> ie.mediaType.equals(mediaType)).findFirst().orElse(null);
+    }
+
 }
